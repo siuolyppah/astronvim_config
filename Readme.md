75f0ec8b3e6a3a325293810e9f94c8bdd9785e08
Gxy <2793392818@qq.com>
2023-10-10 13:57:58 +0800

    feat: set colorscheme to `catppuccin`

--- Readme.md
+++ Readme.md
@@ -12,11 +12,18 @@
     git clone https://github.com/siuolyppah/astronvim_config.git ~/.config/nvim/lua/user
     ```
 
-3. Modify the `~/.config/nvim/lua/user` folder and 
+3. Initlialize AstroNvim
+   
+    ```
+    nvim --headless -c 'quitall'
+    ```
+
+4. Modify the `~/.config/nvim/lua/user` folder and 
     `user/init.lua` file to your liking and push the changes you make to your repository
 
 
 # Related
 
-- [AstroNvim Configuration](https://docs.astronvim.com/#%EF%B8%8F-configuration)
-
+- [Setup AstroNvim Configuration](https://docs.astronvim.com/#%EF%B8%8F-configuration)
+- [Configuration Example](https://github.com/AstroNvim/astrocommunity#-setup)
+- [Custom Colorscheme](https://docs.astronvim.com/recipes/colorscheme)
---
+++ plugins/community.lua
@@ -0,0 +1,30 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        rainbow_delimiters = true,
+      },
+    },
+  },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 120,
+      disabled_filetypes = { "help" },
+    },
+  },
+}
